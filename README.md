2b2b62ad268856c6e0595c807a1e3e7e34fe3261
Neelakshi <neelakshikadyan@gmail.com>
2025-10-01 22:51:39 +0530

    different selectors

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 ## writing my first e2e test
 
 --- describe()  --> just a conventional thing, doesn't have a functional value
+--- headless cypress without needing GUI -> npx cypress run --headless
+--- Cypress supports partial testing, i.e. suppose you visit a page and you are trying to match text present on that page , then even if some words are compared it will pass the test
+
+## learned different ways to select data
+
+--- contains
+--- get(using selectors)
+--- using timeout where the content might take time to load
--- cypress/e2e/hello-world.cy.js
+++ cypress/e2e/hello-world.cy.js
@@ -1,8 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Empty Test', () => {
+describe('Basic tests', () => {
     it('test one',()=>{
         cy.visit('https://codedamn.com')
+    })
+
+    it('test two',()=>{
+        cy.visit('https://nidhi8595.github.io/HTML_CSS_PROJECTS/Portfolio/')
+        cy.contains('Portfolio', { timeout: 10000 }).should('be.visible') // way 1:- don't use this way if you don't want your test to fail in future if text changes
+
+        cy.get('div > a') // way 2
+
+        cy.get('[id=skills]') // way 3:- best way in case the style or text changes
+
+        
     })
     
 })
